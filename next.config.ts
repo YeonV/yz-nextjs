f6c3d776d7da00c50f969f0bf7d77c2872a673fd
YeonV <trusted.yeon@gmail.com>
2024-10-22 02:38:45 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.GHPAGES ? 'export' : 'standalone',
-  images: process.env.GHPAGES ? { loader: 'custom', loaderFile: './my-loader.ts' } : {},
   basePath: '/yz-nextjs'
 }
 
