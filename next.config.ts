3584d059d94cd5029aa3a1e75e65f80f7116cd1f
YeonV <trusted.yeon@gmail.com>
2024-10-22 03:04:59 +0200

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.GHPAGES ? 'export' : 'standalone',
-  basePath: '/yz-nextjs'
+  basePath: process.env.GHPAGES ? '/yz-nextjs' : ''
 }
 
 export default nextConfig
